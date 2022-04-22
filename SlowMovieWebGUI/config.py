0fb67d164480c5cb96e282e03b47c15d86d51d60
Tim <info@tmcrafz.org>
2022-04-22 23:59:32 +0200

    Add service file

---
+++ SlowMovieWebGUI/config.py
@@ -0,0 +1,8 @@
+# IP of your device
+HOST = "192.168.30.1"
+PORT = 6060
+
+# Path to SlowMovie application
+SLOW_MOVIE_EXECUTION_PATH = "/home/pi/SlowMovie/slowmovie.py"
+# Path to SlowMovie movies
+SLOW_MOVIE_VIDEO_PATH = "/home/pi/SlowMovie/Videos"
